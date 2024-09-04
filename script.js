9f400821ad1f793e90a76485071040f52080c6db
vekosek <vika_klyueva@inbox.ru>
2024-09-05 02:13:31 +0700

    Added visual tasks and deleting ability

--- script.js
+++ script.js
@@ -1,33 +1,94 @@
-class Case {
+class Task {
     constructor(name, description='', flag=false) {
         this.name = name;
         this.description = description;
         this.flag=flag;
+        this.makeTaskDiv();
+    }
+
+    // Создание элемента div для дела 
+    makeTaskDiv() {
+        let id = 'task' + currentId.toString();
+        currentId += 1;
+    
+        // Создание task-div
+        var newTaskDiv = document.createElement('div');
+        newTaskDiv.className = 'task-div';
+        newTaskDiv.id = id;
+        document.querySelector('.tasks-divs').appendChild(newTaskDiv);
+    
+        // Создание task-info внутри task-div
+        var taskInfoDiv = document.createElement('div');
+        taskInfoDiv.className = 'task-info';
+        taskInfoDiv.id = 'task-info' + id;
+        document.getElementById(id).appendChild(taskInfoDiv);
+    
+        // Создание кнопки "Удалить" внутри task-div
+        var deleteButton = document.createElement('button');
+        deleteButton.className = 'delete';
+        deleteButton.id = 'delete' + id;
+        deleteButton.innerHTML = 'Удалить';
+        document.getElementById(id).appendChild(deleteButton);
+
+        // Удаление дела по нажатию на кнопку
+        deleteButton.onclick = function() {deleteTask(id)}
+    
+        // Создание чекбокса внутри task-info
+        var checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.id = 'checbox' + id;
+        document.getElementById('task-info' + id).appendChild(checkbox);
+
+        // Замена состояния flag при нажатии на чекбокс
+        checkbox.onclick = () => {this.flag = !this.flag};
+    
+        // Создание имени дела внутри task-info
+        var taskNameP = document.createElement('p');
+        taskNameP.className = 'task-name';
+        taskNameP.id = 'taskName' + id;
+        taskNameP.innerHTML = this.name;
+        document.getElementById('task-info' + id).appendChild(taskNameP);
+    
+        // Создание описания дела внутри task-info
+        var taskDescriptionP = document.createElement('p');
+        taskDescriptionP.className = 'task-description';
+        taskDescriptionP.id = 'taskDescriptionP' + id;
+        taskDescriptionP.innerHTML = this.description;
+        document.getElementById('task-info' + id).appendChild(taskDescriptionP);
     }
 }
 
 // Добавление дела
-function addCase() {
-    let inputName = document.querySelector('input[name="caseInput"]')
-    let inputDescription = document.querySelector('input[name="caseDescription"]')
+function addTask() {
+    let inputName = document.querySelector('input[name="taskInput"]');
+    let inputDescription = document.querySelector('input[name="taskDescription"]');
+
     // Получение текста из полей
-    let caseName = inputName.value;
-    let caseDescription = inputDescription.value;
-    // Создание и дела
-    let newCase = new Case(caseName, caseDescription);
-    cases.push(newCase);
+    let taskName = inputName.value;
+    let taskDescription = inputDescription.value;
+
+    // Создание дела с обработкой их количества
+    if (count < 10) {
+        new Task(taskName, taskDescription);
+        count += 1;
+    }
+    else {
+        window.alert('Слишком много задач');
+    }
 
     // Очистка текстовых полей
     inputName.value = "";
     inputDescription.value = "";
-
-    console.log(cases)
 }
 
 // Удаление элемента из списка
-function deleteCase(index) {
-    cases.splice(index, 1);
+function deleteTask(id) {
+    var task = document.getElementById(id);
+    task.remove();
+    count -= 1;
 }
 
-document.getElementById('add').onclick = addCase;
-var cases = []
+document.getElementById('add').onclick = addTask;
+
+var count = 0;
+var currentId = 0
